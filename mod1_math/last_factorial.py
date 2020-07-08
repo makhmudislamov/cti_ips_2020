df2f18250fc1693200ead1cc3d9dd30bdc093717
Makhmud Islamov <sunnatovichvv@gmail.com>
2020-07-07 18:27:58 -0700

    started

---
+++ mod1_math/last_factorial.py
@@ -0,0 +1,21 @@
+"""
+Instructions from your teacher:
+Prompt:
+Given a non-negative number, N, return the last digit of the factorial of N.
+
+
+
+The factorial of N, which is written as N!, is defined as the product of all of the integers from 1 to N.
+
+Given 3 as N, the factorial is 1 x 2 x 3 = 6
+Given 6 as N, the factorial is 1 x 2 x 3 x 4 x 5 x 6 = 720
+Given 9 as N, the factorial is 1 x 2 x 3 x 4 x 5 x 6 x 7 x 8 x 9 = 362,880
+
+As you can see, the number can grow to be quite large, quite quickly.
+
+Write a function that will return only the last digit of N!, given N.
+"""
+
+
+def last_factorial_digit(n):
+    pass
