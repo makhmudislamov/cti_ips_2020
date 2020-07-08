c821bc92d89eded2af1acd3fec54f575c5b887eb
Makhmud Islamov <sunnatovichvv@gmail.com>
2020-07-07 18:38:47 -0700

    started

--- mod1_math/power_of_3.py
+++ mod1_math/power_of_3.py
@@ -19,4 +19,26 @@ Output: false
 
 
 def power_of_three(n):
+  # FOUND THIS HERE: https://www.geeksforgeeks.org/find-whether-given-integer-power-3-not/
+    """ The maximum power of 3 value that  
+     integer can hold is 1162261467 ( 3^19 ) ."""
+    # if 1162261467 % n == 0:
+    #   return True
+    # return False
+
+    # MY SOLUTION >> test at n = 243 was not passing because log_base_3 = 4.999999999999
+    # getting log3(n)
+    # log_base_3 = math.log(n, 3)
+    # # check if log_base_3 is a whole num
+    # if log_base_3 % 2 == 0 or (log_base_3 + 1) % 2 == 0:
+    #     return True
+    # return False
+
+    # ANOTHER SOLUTION I MADE:
+
+    while n > 1:
+      n /= 3
+
+    if n == 1:
+        return True
     return False
