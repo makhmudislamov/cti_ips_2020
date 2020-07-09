a27384f2793d90be424ee4298c052806f95ad26f
Makhmud Islamov <sunnatovichvv@gmail.com>
2020-07-08 20:58:01 -0700

    done

--- mod2_lists/1st_missing_int.py
+++ mod2_lists/1st_missing_int.py
@@ -13,4 +13,23 @@ Your algorithm should run in O(n) time.
 
 
 def first_missing_positive_integer(integers):
-    pass
+    found_int = []
+
+    for integer in integers:
+        if integer < 0:
+            continue
+        if integer + 1 > len(found_int):
+            extend_size = integer - len(found_int) + 1
+            found_int.extend([False] * extend_size)
+        found_int[integer] = True
+
+    missing_int = 0
+    for position in range(1, len(found_int)):
+        if found_int[position] == False:
+            missing_int = position
+        return missing_int
+
+    if missing_int == 0 and len(found_int) == 0:
+        return 1
+    elif missing_int == 0 and len(found_int) > 0:
+        return len(found_int)
