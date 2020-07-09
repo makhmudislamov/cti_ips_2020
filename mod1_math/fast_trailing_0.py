ee1471120c2118b62f242548be849c0a1e4bbcfb
Makhmud Islamov <sunnatovichvv@gmail.com>
2020-07-08 20:22:32 -0700

    done

--- mod1_math/fast_trailing_0.py
+++ mod1_math/fast_trailing_0.py
@@ -18,7 +18,12 @@ https://docs.google.com/spreadsheets/d/1C6ln6zfmG5Q92fHJosjZxe0WuSS0x_WThzBtc5_n
 
 def fast_trailing_zero_factorial(n):
 
-    return n
+    # The pattern shows that trailin zero amount is equal to how many times n can be divided by 5.
+    if n < 5:
+        return 0
+    else:
+        trailing_num = n // 5
+    return trailing_num
 
 
 print("Enter number")
