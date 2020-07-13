2eca1e0bb4423e17c9f456f4f6f1d93f01be273b
Makhmud Islamov <sunnatovichvv@gmail.com>
2020-07-12 20:02:31 -0700

    started

---
+++ mod3_lists/single_num.py
@@ -0,0 +1,31 @@
+"""
+Given a non-empty array of integers, every element appears twice except for one. Find that single one.
+
+Note:
+Your algorithm should have a linear runtime complexity. Could you implement it without using extra memory?
+
+
+Example 1:
+Input: [2,2,1]
+Output: 1
+
+Example 2:
+Input: [4,1,2,1,2]
+Output: 4
+
+
+
+There are several approaches to this problem. Start with one that makes sense to you- so long as you pass all the tests. 
+
+
+Remember to try to solve the problem first by using the most naive solution, then start asking yourself questions about the operations you're doing to try to reduce the amount of time and space used. 
+
+Below are a few hints that describe each approach. 
+
+Try to get the first one working, then take a look at a few of the approaches.
+
+"""
+
+
+def single_number(integers):
+    pass
