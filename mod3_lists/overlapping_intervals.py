d3bf703362f7c00bf80baec50b870b2086b04739
Makhmud Islamov <sunnatovichvv@gmail.com>
2020-07-12 20:11:30 -0700

    done

--- mod3_lists/overlapping_intervals.py
+++ mod3_lists/overlapping_intervals.py
@@ -13,4 +13,17 @@ Make sure the returned intervals are sorted.
 
 
 def merge_overlapping_intervals(intervals):
-    pass
+  if len(intervals) < 2:
+      return intervals
+
+  index = 0
+  while index < len(intervals) - 1:
+    #   check if the last item of an array is greater or equal to the first item of next array
+      if intervals[index][1] > intervals[index + 1][0]:
+          del intervals[index][1]
+          del intervals[index + 1][0]
+          intervals[index].extend(intervals[index + 1])
+          del intervals[index + 1]
+      else:
+          index += 1
+  return intervals
