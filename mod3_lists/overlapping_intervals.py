2c770612587a85046c5c0fc147ff44646ba2cc9e
Makhmud Islamov <sunnatovichvv@gmail.com>
2020-07-12 20:10:42 -0700

    started

---
+++ mod3_lists/overlapping_intervals.py
@@ -0,0 +1,16 @@
+"""
+Given a collection of intervals, merge all overlapping intervals.
+
+These intervals are sorted by the starting index; [[1, 4], [2, 3]] is fine but [[2, 3], [1, 4]] is not.
+
+For example:
+Given [1,3],[2,6],[8,10],[15,18],
+
+return [1,6],[8,10],[15,18].
+
+Make sure the returned intervals are sorted.
+"""
+
+
+def merge_overlapping_intervals(intervals):
+    pass
