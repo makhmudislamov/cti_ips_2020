b8ef24407bb574749d5e452299825008631c04e0
Makhmud Islamov <sunnatovichvv@gmail.com>
2020-07-12 20:14:46 -0700

    started

---
+++ mod3_lists/wave_arr.py
@@ -0,0 +1,13 @@
+"""
+Instructions from your teacher:
+Given an array with three integers, sort the array into a wave like array and return it, 
+In other words, arrange the elements into a sequence such that a[1] >= a[2] <= a[3] 
+
+You can assume the array is sorted.
+Example
+Given [1, 2, 3]
+
+Output return: [2, 1, 3]
+"""
+def wave_array_3(integers):
+    pass
