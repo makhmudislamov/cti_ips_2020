f4945fdb32156d1dc5b087f0cbd6075d746f1e17
Makhmud Islamov <sunnatovichvv@gmail.com>
2020-07-14 19:53:46 -0700

    done

--- mod5_search/merge_sorted_arr.py
+++ mod5_search/merge_sorted_arr.py
@@ -12,3 +12,17 @@ nums2 = [2,5,6],       n = 3
 
 Output: [1,2,2,3,5,6]
 """
+
+
+def merge_sorted_list(nums1, nums2):
+    # modify num1 in-place
+    nums1_indx = 0
+    nums2_indx = 0
+
+    while nums2_indx < len(nums2):
+        if nums2[nums2_indx] <= nums1[nums1_indx] or nums1[nums1_indx] == 0:
+            nums1.insert(nums1_indx, nums2[nums2_indx])
+            nums1.pop()
+            nums2_indx += 1
+        else:
+            nums1_indx += 1
