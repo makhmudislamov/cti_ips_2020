20edc784e9a60f171862b50c65a006d98f68489d
Makhmud Islamov <sunnatovichvv@gmail.com>
2020-07-14 21:51:33 -0700

    started

---
+++ mod6_sorting_algos/merge_sort.py
@@ -0,0 +1,7 @@
+"""
+Given a list of unsorted numbers, sort them using the Merge Sort algorithm. 
+
+Don't use the built-in sorted or list.sort() methods - the goal of this is to understand and implement the merge sort algorithm.
+"""
+
+
