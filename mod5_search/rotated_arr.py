92ef2e4254238d005db8dfadd61c38829dff435a
Makhmud Islamov <sunnatovichvv@gmail.com>
2020-07-14 21:41:51 -0700

    started

---
+++ mod5_search/rotated_arr.py
@@ -0,0 +1,11 @@
+"""
+Suppose a sorted array A is rotated at some pivot unknown to you beforehand.
+(i.e., 0 1 2 4 5 6 7 might become 4 5 6 7 0 1 2).
+Find the minimum element.
+The array will not contain duplicates.
+
+Note: If you know the number of times the array is rotated, then this problem becomes trivial. If the number of rotation is x, then minimum element is A[x].
+
+Hints on the original problem: https://www.interviewbit.com/problems/rotated-array/
+"""
+
