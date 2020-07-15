b810aabcffa00c35601a0667df7bf550c988b92f
Makhmud Islamov <sunnatovichvv@gmail.com>
2020-07-14 21:44:48 -0700

    done

--- mod5_search/find_indx.py
+++ mod5_search/find_indx.py
@@ -9,3 +9,19 @@ Examples:
 [1,3,5,6], 7 → 4
 [1,3,5,6], 0 → 0
 """
+
+
+def find_index(sorted_list, target):
+  start = 0
+  end = len(sorted_list)
+
+  while start < end:
+    pivot = start + (end - start) // 2
+
+    if sorted_list[pivot] < target:
+      start = pivot + 1
+      # print(sorted_list[start])
+    else:
+      end = pivot
+
+  return start
