afa8f2895a9a9b3a86db9168b16648571ea27409
Makhmud Islamov <sunnatovichvv@gmail.com>
2020-08-12 12:45:45 -0700

    done

--- mod9_linkedlist/ll_length.py
+++ mod9_linkedlist/ll_length.py
@@ -9,3 +9,47 @@ ll.extend([1, 2, 3, 4, 5])
 ll.length() # returns 5
 """
 
+### do not modify this class
+
+
+class LinkedNode:
+  def __init__(self, data):
+    self.data = data
+    self.next = None
+
+### do not modify this class, or any of the methods in it, other than length()
+### you may insert new methods if you like
+
+
+class LinkedList:
+  def __init__(self):
+    self.head = None
+    self.tail = None
+
+  def empty(self):
+    return self.head == None
+
+  def append(self, data):
+    if self.empty():
+      self.head = LinkedNode(data)
+      self.tail = self.head
+    else:
+      new_node = LinkedNode(data)
+      self.tail.next = new_node
+      self.tail = new_node
+
+  def extend(self, array):
+    for element in array:
+      self.append(element)
+
+  # implement this method
+  # return the length of the linked list, an integer value
+  def length(self):
+    ll_len = 1
+    curr_node = self.head
+    if self.empty():
+      return 0
+    while curr_node.next != None:
+      curr_node = curr_node.next
+      ll_len += 1
+    return ll_len
