2dcff288513610ee9187c72c1a813a47a128033b
Makhmud Islamov <sunnatovichvv@gmail.com>
2020-08-12 12:41:01 -0700

    started

---
+++ mod8_hashmaps/word_count.py
@@ -0,0 +1,37 @@
+"""
+Given a long text string, count the number of occurrences of each word. Ignore case. Assume the boundary of a word is whitespace - a " ", or a line break denoted by "\n". Ignore all punctuation, such as . , ~ ? !. Assume hyphens are part of a word - "two-year-old" and "two year old" are one word, and three different words, respectively. 
+
+Return the word counts as a string formatted with line breaks, in no particular order.
+
+Example:
+"I do not like green eggs and ham,
+I do not like them, Sam-I-Am"
+
+Output:
+i 2
+do 2
+not 2
+like 2
+green 1
+eggs 1
+and 1
+ham 1
+them 1
+sam-i-am 1
+
+Also Valid:
+and 1
+do 2
+eggs 1
+green 1
+ham 1
+i 2
+like 2
+not 2
+sam-i-am 1
+them 1
+"""
+
+
+def count_words(text):
+    pass
