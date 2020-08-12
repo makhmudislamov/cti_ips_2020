e297ef40f7349f0348a8432569121e0f0172436a
Makhmud Islamov <sunnatovichvv@gmail.com>
2020-08-12 12:59:55 -0700

    done

--- mod9_linkedlist/insert_node.py
+++ mod9_linkedlist/insert_node.py
@@ -13,10 +13,7 @@ The list would then have nodes ordered like this: 1,2,4,3,4,5,6
 For the above example, it would be valid to insert an item at index 6. Inserting an item at an index equal to the list's length is the same as appending a new item to the list. Additionally, it is valid to insert an item at index 0, which puts that item at the head of the list. Make sure to account for these edge cases in your code.
 
 """
-
 ### do not modify this class
-
-
 class LinkedNode:
   def __init__(self, data):
     self.data = data
@@ -24,16 +21,14 @@ class LinkedNode:
 
 ### do not modify this class, or any of the methods in it, other than insert()
 ### you may insert new methods if you like
-
-
 class LinkedList:
   def __init__(self):
     self.head = None
     self.tail = None
-
+    
   def empty(self):
     return self.head == None
-
+    
   def append(self, data):
     if self.empty():
       self.head = LinkedNode(data)
@@ -42,11 +37,11 @@ class LinkedList:
       new_node = LinkedNode(data)
       self.tail.next = new_node
       self.tail = new_node
-
+      
   def extend(self, array):
     for element in array:
       self.append(element)
-
+  
   # used in test cases to verify solutions
   # if you want to test your code without submitting, consider using this function
   def get(self, index):
@@ -56,7 +51,16 @@ class LinkedList:
       curr = curr.next
       count -= 1
     return curr.data
-
+  
   # implement this method
   def insert(self, data, index):
-    pass
+    new_node = LinkedNode(data)
+    if index == 0:
+      new_node.next = self.head
+      self.head = new_node
+    curr_node = self.head
+    for _ in range(index - 1):
+      curr_node = curr_node.next
+    
+    new_node.next = curr_node.next
+    curr_node.next = new_node
