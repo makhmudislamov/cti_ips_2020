7252534717016c3f397c054c77814ecff84c90ba
Makhmud Islamov <sunnatovichvv@gmail.com>
2020-08-12 13:05:31 -0700

    done

--- mod9_linkedlist/min_stack.py
+++ mod9_linkedlist/min_stack.py
@@ -19,36 +19,39 @@ minStack.getMin();   --> Returns -2.
 
 class MinStack(object):
 
-    def __init__(self):
-        """
-        initialize your data structure here.
-        """
-        return
+  def __init__(self):
+      """
+      initialize your data structure here.
+      """
+      self.stack = []
 
-    def push(self, x):
-        """
-        :type x: int
-        :rtype: None
-        """
-        return
+  def push(self, x):
+      """
+      :type x: int
+      :rtype: None
+      """
+      self.stack.append(x)
+      return self.stack
 
-    def pop(self):
-        """
-        :rtype: None
-        """
-        return
+  def pop(self):
+      """
+      :rtype: None
+      """
+      self.stack.pop()
+      return self.stack
 
-    def top(self):
-        """
-        :rtype: int
-        """
-        return
+  def top(self):
+      """
+      :rtype: int
+      """
+      top_element_inx = len(self.stack) - 1
+      return self.stack[top_element_inx]
 
-    def getMin(self):
-        """
-        :rtype: int
-        """
-        return
+  def getMin(self):
+      """
+      :rtype: int
+      """
+      return min(self.stack)
 
 # Your MinStack object will be instantiated and called as such:
 # obj = MinStack()
