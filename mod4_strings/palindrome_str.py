3f1ceab9070822c5e322c0b7732fe6999f840f49
Makhmud Islamov <sunnatovichvv@gmail.com>
2020-08-17 11:47:15 -0700

    started

---
+++ mod4_strings/palindrome_str.py
@@ -0,0 +1,16 @@
+"""
+Given a string, determine if it is a palindrome, considering only alphanumeric characters and ignoring cases.
+
+Return 0 / 1 ( 0 for false, 1 for true ) for this problem.
+
+Examples:
+"Race Car" # returns 1
+
+"Race a Car" # returns 0
+
+"otto" # returns 1
+
+"A man, a plan, a canal, Panama." # returns 1
+"""
+
+
