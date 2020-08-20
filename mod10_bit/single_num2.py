d05aec183d2143d37ab280f6c957fdf1b32521f6
Makhmud Islamov <sunnatovichvv@gmail.com>
2020-08-20 11:49:06 -0700

    started

---
+++ mod10_bit/single_num2.py
@@ -0,0 +1,13 @@
+"""
+Given an array of integers, every element appears thrice except for one which occurs once.
+Find that element which does not appear thrice.
+Note: Your algorithm should have a linear runtime complexity.
+Could you implement it without using extra memory?
+
+Example:
+>> arr = [1, 2, 3, 1, 2, 3, 1, 2, 3, 4]
+>> single_number(arr) # returns 4
+
+OG Platform:  https://www.interviewbit.com/problems/single-number-ii/ 
+"""
+
