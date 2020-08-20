bfd7fa76d65146cbb4d79d1a072359f0c2384c2f
Makhmud Islamov <sunnatovichvv@gmail.com>
2020-08-20 11:34:08 -0700

    done

--- mod10_bit/reverse_bits.py
+++ mod10_bit/reverse_bits.py
@@ -4,3 +4,15 @@ Given an integer n, reverse its first 32 bits, and return the reversed-bit integ
 Example:
 5 (00000000000000000000000000000101) ==> 2684354560 (10100000000000000000000000000000)
 """
+
+
+def reverse_bits(num):
+    nn = 0
+    count = 0
+    while count < 32:
+        v = num % 2
+        num = num >> 1
+        nn = (nn << 1) + v
+        count += 1
+
+    return nn
