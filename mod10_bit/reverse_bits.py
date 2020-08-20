1a17a6bcf54812b988c8af3e6f43e542895b9ee4
Makhmud Islamov <sunnatovichvv@gmail.com>
2020-08-20 11:25:58 -0700

    started

---
+++ mod10_bit/reverse_bits.py
@@ -0,0 +1,6 @@
+"""
+Given an integer n, reverse its first 32 bits, and return the reversed-bit integer.
+0 <= n <= 2^32 - 1
+Example:
+5 (00000000000000000000000000000101) ==> 2684354560 (10100000000000000000000000000000)
+"""
