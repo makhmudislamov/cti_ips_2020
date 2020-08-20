c0d846e942759d302988e2181780237f9ab8ffe1
Makhmud Islamov <sunnatovichvv@gmail.com>
2020-08-20 11:58:10 -0700

    done

--- mod10_bit/single_num2.py
+++ mod10_bit/single_num2.py
@@ -11,3 +11,19 @@ Example:
 OG Platform:  https://www.interviewbit.com/problems/single-number-ii/ 
 """
 
+
+def single_number(nums):
+    ans = 0
+    for i in range(0, 32):
+        count = 0
+        for a in nums:
+            if ((a >> i) & 1):
+                count += 1
+        ans |= ((count % 3) << i)
+    return convert(ans)
+
+
+def convert(x):
+    if x >= 2**31:
+        x -= 2**32
+    return x
