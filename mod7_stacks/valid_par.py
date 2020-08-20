e1e7d43f62cbedf806a9337281603bdc1097267a
Makhmud Islamov <sunnatovichvv@gmail.com>
2020-08-20 10:26:30 -0700

    done

--- mod7_stacks/valid_par.py
+++ mod7_stacks/valid_par.py
@@ -5,3 +5,30 @@ Return 0 / 1 ( 0 for false, 1 for true ) for this problem.
 
 Original Site:  https://www.interviewbit.com/problems/generate-all-parentheses/ 
 """
+
+
+def valid(parentheses):
+    pars = {
+        "(": ")",
+        "[": "]",
+        "{": "}"
+    }
+
+    if len(parentheses) % 2 != 0:
+        return False
+
+    stack = []
+
+    for par in parentheses:
+        if par in pars:
+            stack.append(pars[par])
+        else:
+            if len(stack) == 0:
+                return False
+            current_par = stack.pop()
+            if current_par != par:
+                return False
+
+    if not stack:
+        return True
+    return False
