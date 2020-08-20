30a85eda9ec9ee1047a7912e115c510959c30b33
Makhmud Islamov <sunnatovichvv@gmail.com>
2020-08-20 11:06:55 -0700

    done

--- mod10_bit/divide_int.py
+++ mod10_bit/divide_int.py
@@ -9,3 +9,31 @@ Example 2:
 Input: dividend = 7, divisor = -3
 Output: -2
 """
+
+
+def divide(dividend, divisor):
+    sign = -1 if ((divisor > 0) ^ (dividend > 0)) else 1
+
+# prevent overflow
+    if dividend == -2**31 and divisor == -1:
+        return 2**31-1
+
+    dividend = abs(dividend)
+    divisor = abs(divisor)
+
+    res = 0
+    while divisor <= dividend:
+        div = divisor
+        tmp = 1
+        while(div << 1 <= dividend):
+            tmp <<= 1
+            div <<= 1
+        dividend -= div
+        res += tmp
+    return res*sign
+
+
+testX = 4
+testY = 1
+testResult = divide(testX, testY)
+print(testResult)
