7de2733e37c134c9e203e22f9903fc357b4e27e9
Makhmud Islamov <sunnatovichvv@gmail.com>
2020-08-20 09:38:09 -0700

    started

---
+++ mod7_stacks/backspace_string.py
@@ -0,0 +1,13 @@
+"""
+Given two strings S and T, return if they are equal when both are typed into empty text editors. # means a backspace character.
+
+Note that after backspacing an empty text, the text will continue empty.
+
+Example 1:
+
+Input: S = "ab#c", T = "ad#c"
+Output: true
+Explanation: Both S and T become "ac".
+"""
+
+
