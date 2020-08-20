ef10e9f0d9ba0c61be464e58c3d0ff8494dcb7cb
Makhmud Islamov <sunnatovichvv@gmail.com>
2020-08-20 08:08:06 -0700

    started

---
+++ mod6_sorting_algos/rotate_matrix.py
@@ -0,0 +1,20 @@
+"""
+Given an NxN matrix, rotate it by 90 degrees. You should perform this operation in-place, using only constant memory.
+
+Example:
+starting matrix:
+[
+  [1, 2, 3],
+  [4, 5, 6],
+  [7, 8, 9]
+]
+once rotated by 90 degrees, the matrix becomes:
+ [
+  [7, 4, 1]
+  [8, 5, 2]
+  [9, 6, 3]
+]
+
+Hint:
+Is there an easy way to rotate a coordinate pair by 90 degrees?
+"""
