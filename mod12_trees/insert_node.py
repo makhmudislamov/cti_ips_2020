dbab892c5a5b11584497c6156b72d4b16b45ab36
Makhmud Islamov <sunnatovichvv@gmail.com>
2020-08-22 12:53:30 -0700

    started

---
+++ mod12_trees/insert_node.py
@@ -0,0 +1,5 @@
+"""
+Given a Tree class, create a method called add that accepts a Node, and inserts it into a tree such that the tree continues to be a Binary Search Tree. The method should return nothing.
+
+A Binary Search Tree is a tree where every node has values greater than its data on the right-hand side, and values less than its data on the left-hand side, and all of the sub-tree nodes follow suit. Here's an example:
+"""
